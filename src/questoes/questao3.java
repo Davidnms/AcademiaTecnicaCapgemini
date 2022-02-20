8a7b947c8795355d79c524de3b04ae4e236cc29c
David Nicolas <99841119+Davidnms@users.noreply.github.com>
2022-02-20 19:19:42 -0300

    Update questao3.java

--- src/questoes/questao3.java
+++ src/questoes/questao3.java
@@ -6,9 +6,9 @@ public class questao3 {
 	
 	public static void main(String[]args) {
 		/*
-		 * Pensei muito sobre essa questão e até consegui encontrar a questão no Hacker Rank, mas ainda sim
-		 * não consegui encontrar uma resposta por mim mesmo, a qual conseguisse desenvolver por mim mesmo. Ainda
-		 * sim gostaria de uma chance na Academia, agradeço desde já.
+		 * Pensei muito sobre essa questÃ£o e atÃ© consegui encontrar a questÃ£o no Hacker Rank, mas ainda sim
+		 * nÃ£o consegui encontrar uma resposta a qual conseguisse desenvolver por mim mesmo. Ainda
+		 * sim gostaria de uma chance na Academia, agradeÃ§o desde jÃ¡.
 		*/
 	}
 	
